b68db598f07d8b49d056506a2053b3558807ec4d
ameerthehacker <ameerjhanprof@gmail.com>
2017-09-15 21:49:19 +0530

    Add REST services to delete and update challenges

--- routes/challenges.js
+++ routes/challenges.js
@@ -12,18 +12,7 @@ router.get('/', (req, res, next) => {
         }
     });
 });
-router.get('/:slug', (req, res, next) => {
-    let slug = req.params.slug;
-    Challenge.findBySlug(slug, (err, challenge) => {
-        if(!err) {
-            res.json({ error: false, msg: challenge });
-        }
-        else {
-            res.json({ error: true, msg: err });            
-        }
-    });
-});
-router.post('/create', (req, res, next) => {
+router.post('/', (req, res, next) => {
     let challenge = {};
     challenge.title = req.body.title;    
     challenge.problemStatement = req.body.problemStatement;
@@ -37,6 +26,49 @@ router.post('/create', (req, res, next) => {
 
     newChallenge = new Challenge(challenge);
     newChallenge.save((err) => {
+        if(!err){
+            res.json({ error: false });
+        }
+        else{
+            res.json({ error: true, msg: [err] });            
+        }
+    });
+});
+router.get('/:slug', (req, res, next) => {
+    let slug = req.params.slug;
+    Challenge.findBySlug(slug, (err, challenge) => {
+        if(!err) {
+            res.json({ error: false, msg: challenge });
+        }
+        else {
+            res.json({ error: true, msg: err });            
+        }
+    });
+});
+router.put('/:slug', (req, res, next) => {
+    let slug = req.params.slug;
+    let challenge = {};
+    challenge.title = req.body.title;    
+    challenge.problemStatement = req.body.problemStatement;
+    challenge.inputFormat = req.body.inputFormat;
+    challenge.outputFormat = req.body.outputFormat;
+    challenge.constraints = req.body.constraints;    
+    challenge.sampleInput = req.body.sampleInput;        
+    challenge.sampleOutput = req.body.sampleOutput;    
+    challenge.explanation = req.body.explanation;
+    challenge.testCases = req.body.testCases;
+    Challenge.update({ slug: slug }, challenge, (err) => {
+        if(!err){
+            res.json({ error: false });
+        }
+        else{
+            res.json({ error: true, msg: [err] });            
+        }
+    });
+});
+router.delete('/:slug', (req, res, next) => {
+    let slug = req.params.slug;
+    Challenge.remove({ slug: slug }, (err) => {
         if(!err){
             res.json({ error: false });
         }
