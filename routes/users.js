6a424e7ded72776c9f5b032b50e4d2341b98c4d0
ameerthehacker <ameerjhanprof@gmail.com>
2017-12-14 11:53:56 +0530

    Add REST services to get a user and to update a user

--- routes/users.js
+++ routes/users.js
@@ -14,5 +14,38 @@ router.get('/', function(req, res, next) {
     }
   });
 });
+function initChallengeFromRequest(req) {
+  let user = {};
+  user.firstName = req.body.firstName;
+  user.lastName = req.body.lastName;
+  user.email = req.body.email;
+  user.gender = req.body.gender;
+  return user;
+}
+router.get('/:username', (req, res, next) => {
+  const username = req.params.username;
+
+  User.findByUsername(username, (err, user) => {
+    if(!err) {
+      res.json({ error: false, msg: user });
+    }
+    else {
+      res.json({ error: true, msg: err });
+    }
+  });
+});
+router.put('/:username', (req, res, next) => {
+  const username = req.params.username;
+  let user = initChallengeFromRequest(req);
+
+  User.update({ username: username }, user, (err, user) => {
+    if(!err) {
+      res.json({ error: false, msg: user });
+    }
+    else {
+      res.json({ error: true, msg: err });
+    }
+  });
+});
 
 module.exports = router;
